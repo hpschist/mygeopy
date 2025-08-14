804f0c9cf820b65bc67b04cfa6e5d907ada99a14
hpschist <mullermonster@gmail.com>
2025-08-14 11:28:41 -0800

    fix doctest failure

--- tests/doctest_triangle.py
+++ tests/doctest_triangle.py
@@ -3,10 +3,10 @@ def hypot(a, b):
     Return the hypotenuse of a right triangle.
 
     >>> hypot(3, 4)
-    5
+    5.0
     >>> hypot(5, 12)
-    13
+    13.0
     >>> hypot(-3, -4)
-    5
+    5.0
     """
     return (a**2 + b**2) ** 0.5
